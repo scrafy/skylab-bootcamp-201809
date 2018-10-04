743ac985d588ab3ea362d9f83e53a6d296fdb951
scrafy <scrafy26@gmail.com>
2018-10-04 16:36:04 +0200

    initial

--- staff/joseluis-juste/safe-box/safe-box.js
+++ staff/joseluis-juste/safe-box/safe-box.js
@@ -1,11 +1,17 @@
 function FactorySafeBox(){
 
-    var password = "123";
+    var password = "abc";
     var secret;
     
     return{
         saveSecret:function(_secret, _pass){
             
+            if (_pass === "") throw Error("invalid password");
+
+            if (_pass === undefined) throw Error("invalid password");
+
+            if (_pass.trim() === undefined) throw Error("invalid password");
+
             if (_pass === password){
                 secret = _secret;
                 return;
