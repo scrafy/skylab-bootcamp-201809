a8f202e2d86ea9a4d5fccadcc990a809cb4bbc8d
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-31 12:17:54 +0100

    finish

--- staff/aleix-caminal/learnyounode/uppercaser.js
+++ staff/aleix-caminal/learnyounode/uppercaser.js
@@ -1,1 +1,15 @@
-var http = require('http')
+const http = require('http')
+
+const { argv: [, , port] } = process
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        let content = ''
+
+        req.on('data', chunk => content += chunk)
+
+        req.on('end', () => res.end(content.toUpperCase()))
+    }
+})
+
+server.listen(port)
