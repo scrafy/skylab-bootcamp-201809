c9c3011aa002aa170edc49988c1001f6943670a7
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-31 15:45:11 +0100

    sender

--- staff/aleix-caminal/web-app/index.js
+++ staff/aleix-caminal/web-app/index.js
@@ -1,4 +1,5 @@
-const app = require('express')()
+const express = require('express')
+const app = express();
 
 const { argv: [, , port] } = process
 const users = []
@@ -15,61 +16,60 @@ function parseData(data) {
     return result
 }
 
-
 app.get('/', (req, res) => {
     res.send(
-        `<!DOCTYPE html>
-        <html>
-            <head>
-                <title>Hello World!</title>
-            </head>
-            <body>
-                <h1>Hello World!</h1>
-                <a href="/login">Login</a> or <a href="/register">Register</a>
-            </body>
-        </html>`
+`<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <a href="/login">Login</a> or <a href="/register">Register</a>
+    </body>
+</html>`
     )
 })
 
 app.get('/register', (req, res) => {
     res.send(
-        `<!DOCTYPE html>
-        <html>
-            <head>
-                <title>Hello World!</title>
-            </head>
-            <body>
-                <h1>Hello World!</h1>
-                <form action="/register" method="POST">
-                    <input type="text" name="name" placeholder="Name">
-                    <input type="text" name="surname" placeholder="Surname">
-                    <input type="text" name="username" placeholder="username">
-                    <input type="password" name="password" placeholder="password">
-                    <button type="submit">Register</button>
-                </form>
-                <a href="/">go back</a>
-            </body>
-        </html>`
+`<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <form action="/register" method="POST">
+            <input type="text" name="name" placeholder="Name">
+            <input type="text" name="surname" placeholder="Surname">
+            <input type="text" name="username" placeholder="username">
+            <input type="password" name="password" placeholder="password">
+            <button type="submit">Register</button>
+        </form>
+        <a href="/">go back</a>
+    </body>
+</html>`
     )
 })
 
 app.get('/login', (req, res) => {
     res.send(
-        `<!DOCTYPE html>
-        <html>
-            <head>
-                <title>Hello World!</title>
-            </head>
-            <body>
-                <h1>Hello World!</h1>
-                <form action="/login" method="POST">
-                    <input type="text" name="username" placeholder="username">
-                    <input type="password" name="password" placeholder="password">
-                    <button type="submit">Login</button>
-                </form>
-                <a href="/">go back</a>
-            </body>
-        </html>`
+`<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <form action="/login" method="POST">
+            <input type="text" name="username" placeholder="username">
+            <input type="password" name="password" placeholder="password">
+            <button type="submit">Login</button>
+        </form>
+        <a href="/">go back</a>
+    </body>
+</html>`
     )
 })
 
@@ -80,34 +80,34 @@ app.get('/logout', (req, res) => {
 
 app.get('/home', (req, res) => {
     res.send(
-        `<!DOCTYPE html>
-        <html>
-            <head>
-                <title>Hello World!</title>
-            </head>
-            <body>
-                <h1>Welcome ${auth.name}!</h1>
-                <a href="/logout">logout</a>
-            </body>
-        </html>`
+`<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>Welcome ${auth.name}!</h1>
+        <a href="/logout">logout</a>
+    </body>
+</html>`
     )
 })
 
 app.get('/users', (req, res) => {
     res.send(
-        `<!DOCTYPE html>
-        <html>
-            <head>
-                <title>Hello World!</title>
-            </head>
-            <body>
-                <h1>Hello World!</h1>
-                <ul>
-                    ${users.map(user => `<li>${user.id} ${user.name} ${user.surname}</li>`).join('')}
-                </ul>
-                <a href="/">go back</a>
-            </body>
-        </html>`
+`<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <ul>
+            ${users.map(user => `<li>${user.id} ${user.name} ${user.surname}</li>`).join('')}
+        </ul>
+        <a href="/">go back</a>
+    </body>
+</html>`
     )
 })
 
