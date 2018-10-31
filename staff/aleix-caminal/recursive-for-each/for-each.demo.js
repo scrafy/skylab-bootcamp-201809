66bb40d525b8e3cc86a20e1af778a14a4b7f3110
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-31 15:49:41 +0100

    froear

--- staff/aleix-caminal/recursive-for-each/for-each.demo.js
+++ staff/aleix-caminal/recursive-for-each/for-each.demo.js
@@ -1,10 +1,6 @@
-// for-each.demo.js
-
 const forEach = require('./for-each')
-
 const nums = [1, 2, 3]
 
-forEach(nums, (num, index) => console.log(num))
-// 1
-// 2
-// 3
+forEach(nums, (num, index) => {
+    console.log(num, index)
+})
--- staff/aleix-caminal/recursive-for-each/for-each.js
+++ staff/aleix-caminal/recursive-for-each/for-each.js
@@ -1,7 +1,7 @@
 function forEach(array, callback, index = 0) {
     if (index < array.length) {
         callback(array[index], index)
-
+        
         forEach(array, callback, ++index)
     }
 }
