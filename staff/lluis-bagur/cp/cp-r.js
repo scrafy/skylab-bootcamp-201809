8808871dfcacb917c904a97d1a05f177af046ca5
Lluis Bagur <lluisbagur@MacBook-Pro-de-Lluis.local>
2018-10-31 09:59:50 +0100

    cp-r

---
+++ staff/lluis-bagur/cp/cp-r.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const { argv: [, , from, to] } = process
+
+
+    fs.mkdirSync(to);
+    fs.readdirSync(from).forEach(element => {
+        if (fs.lstatSync(path.join(from, element)).isFile()) {
+            fs.copyFileSync(path.join(from, element), path.join(to, element));
+        } else {
+            copyFolderSync(path.join(from, element), path.join(to, element));
+        }
+    })
+
