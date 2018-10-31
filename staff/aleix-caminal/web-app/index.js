059e75db413ffac21df13f1edb1144e50f90ccb9
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-31 14:03:02 +0100

    login & logout

--- staff/aleix-caminal/web-app/index.js
+++ staff/aleix-caminal/web-app/index.js
@@ -1,60 +1,114 @@
-const express = require('express')
+const app = require('express')()
 
 const { argv: [, , port] } = process
-
-const app = express()
-
 const users = []
 
-app.get('/', (req, res) => {
-    res.send(`<!DOCTYPE html>
-<html>
-    <head>
-        <title>Hello World!</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-        <a href="/login">Login</a> or <a href="/register">Register</a>
-    </body>
-</html>`)
-})
+let auth = {}
 
-app.get('/login', (req, res) => {
-    res.send(`<!DOCTYPE html>
-<html>
-    <head>
-        <title>Hello World!</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-        <form action="/..." method="POST">
-            <input type="text" name="username" placeholder="username">
-            <input type="password" name="password" placeholder="password">
-            <button type="submit">Login</button>
-        </form>
-        <a href="/">go back</a>
-    </body>
-</html>`)
+function parseData(data) {
+    let result = {}
+    data.split('&').forEach(keyValue => {
+        const [key, value] = keyValue.split('=')
+        result[key] = value
+    })
+
+    return result
+}
+
+
+app.get('/', (req, res) => {
+    res.send(
+        `<!DOCTYPE html>
+        <html>
+            <head>
+                <title>Hello World!</title>
+            </head>
+            <body>
+                <h1>Hello World!</h1>
+                <a href="/login">Login</a> or <a href="/register">Register</a>
+            </body>
+        </html>`
+    )
 })
 
 app.get('/register', (req, res) => {
-    res.send(`<!DOCTYPE html>
-<html>
-    <head>
-        <title>Hello World!</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-        <form action="/register" method="POST">
-            <input type="text" name="name" placeholder="Name">
-            <input type="text" name="surname" placeholder="Surname">
-            <input type="text" name="username" placeholder="username">
-            <input type="password" name="password" placeholder="password">
-            <button type="submit">Register</button>
-        </form>
-        <a href="/">go back</a>
-    </body>
-</html>`)
+    res.send(
+        `<!DOCTYPE html>
+        <html>
+            <head>
+                <title>Hello World!</title>
+            </head>
+            <body>
+                <h1>Hello World!</h1>
+                <form action="/register" method="POST">
+                    <input type="text" name="name" placeholder="Name">
+                    <input type="text" name="surname" placeholder="Surname">
+                    <input type="text" name="username" placeholder="username">
+                    <input type="password" name="password" placeholder="password">
+                    <button type="submit">Register</button>
+                </form>
+                <a href="/">go back</a>
+            </body>
+        </html>`
+    )
+})
+
+app.get('/login', (req, res) => {
+    res.send(
+        `<!DOCTYPE html>
+        <html>
+            <head>
+                <title>Hello World!</title>
+            </head>
+            <body>
+                <h1>Hello World!</h1>
+                <form action="/login" method="POST">
+                    <input type="text" name="username" placeholder="username">
+                    <input type="password" name="password" placeholder="password">
+                    <button type="submit">Login</button>
+                </form>
+                <a href="/">go back</a>
+            </body>
+        </html>`
+    )
+})
+
+app.get('/logout', (req, res) => {
+    auth = {}
+    res.redirect('/login')
+})
+
+app.get('/home', (req, res) => {
+    res.send(
+        `<!DOCTYPE html>
+        <html>
+            <head>
+                <title>Hello World!</title>
+            </head>
+            <body>
+                <h1>Welcome ${auth.name}!</h1>
+                <a href="/logout">logout</a>
+            </body>
+        </html>`
+    )
+})
+
+app.get('/users', (req, res) => {
+    res.send(
+        `<!DOCTYPE html>
+        <html>
+            <head>
+                <title>Hello World!</title>
+            </head>
+            <body>
+                <h1>Hello World!</h1>
+                <ul>
+                    ${users.map(user => `<li>${user.id} ${user.name} ${user.surname}</li>`).join('')}
+                </ul>
+                <a href="/">go back</a>
+            </body>
+        </html>`
+    )
 })
 
 app.post('/register', (req, res) => {
@@ -63,46 +117,23 @@ app.post('/register', (req, res) => {
     req.on('data', chunk => data += chunk)
 
     req.on('end', () => {
-        const keyValues = data.split('&')
-
-        const user = { id: Date.now() }
-
-        keyValues.forEach(keyValue => {
-            const [key, value] = keyValue.split('=')
-
-            user[key] = value
-        })
-
+        const user = parseData(data)
+        user.id = Date.now()
         users.push(user)
-
-        res.send(`<!DOCTYPE html>
-<html>
-    <head>
-        <title>Hello World!</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-        <p>Ok! user ${user.name} registered.
-        <a href="/">go back</a>
-    </body>
-</html>`)
+        res.redirect('/login')
     })
 })
 
-app.get('/users', (req, res) => {
-    res.send(`<!DOCTYPE html>
-<html>
-    <head>
-        <title>Hello World!</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-        <ul>
-            ${users.map(user => `<li>${user.id} ${user.name} ${user.surname}</li>`).join('')}
-        </ul>
-        <a href="/">go back</a>
-    </body>
-</html>`)
+app.post('/login', (req, res) => {
+    let data = ''
+
+    req.on('data', chunk => data += chunk)
+
+    req.on('end', () => {
+        const login = parseData(data)
+        auth = users.find(user => user.username === login.username && user.password === login.password) || {}
+        res.redirect('/home')
+    })
 })
 
 app.listen(port || 3000)
