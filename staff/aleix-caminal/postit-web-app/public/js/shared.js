88a435f0819197d88cb27aa0c4f9a3b3f110d74c
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-11-07 11:35:48 +0100

    validate

--- staff/aleix-caminal/postit-web-app/public/js/shared.js
+++ staff/aleix-caminal/postit-web-app/public/js/shared.js
@@ -2,14 +2,14 @@ function validate(form, inputs) {
     if (typeof form !== 'object' || form.tagName !== 'FORM') throw Error('no form passed as argument')
     if (!Array.isArray(inputs) || inputs.length < 1) throw Error('array is not valid')
 
-    var result = 1;
+    let result = true;
     for (var i in inputs) {
-        var input = form.querySelector('input[name="' + inputs[i] + '"]');
+        const input = form.querySelector('input[name="' + inputs[i] + '"]')
         if (!input.value) {
-            input.classList.add('is-invalid');
-            result = 0;
+            input.classList.add('is-invalid')
+            result = false
         } else {
-            input.classList.remove('is-invalid');
+            input.classList.remove('is-invalid')
         }
     }
 
