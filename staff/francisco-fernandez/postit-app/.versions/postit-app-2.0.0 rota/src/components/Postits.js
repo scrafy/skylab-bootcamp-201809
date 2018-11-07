7efccd6cc99f0dc028d6b6487c5a6dd58d7a9d08
Paco <franfercant@gmail.com>
2018-11-07 13:53:28 +0100

    postit-app fixed

--- staff/francisco-fernandez/postit-app/.versions/postit-app-2.0.0 rota/src/components/Postits.js
+++ staff/francisco-fernandez/postit-app/.versions/postit-app-2.0.0 rota/src/components/Postits.js
@@ -7,36 +7,42 @@ class Postits extends Component {
     state = { postits: [] }
 
     componentDidMount() {
-        logic.listPostits()
-            .then(postits => { this.setState({ postits }) })
+        console.log('Postits', 'componentDidMount')
 
-        // TODO error handling!
+        const { userId, token } = this.props
+
+        logic.listPostitsByUser(userId, token)
+            .then(postits => { this.setState({ postits }) })
     }
 
-    handleSubmit = text =>
-        logic.createPostit(text)
-            .then(() => logic.listPostits())
+    handleSubmit = text => {
+        const { userId, token } = this.props
+
+        logic.createPostit(text, userId, token)
+            .then(() => logic.listPostitsByUser(userId, token))
+            .then(postits => this.setState({ postits }))
+    }
+
+    handleDeletePost = id => {
+        const { userId, token } = this.props
+
+        logic.deletePostit(id, userId, token)
+            .then(() => logic.listPostitsByUser(userId, token))
+            .then(postits => this.setState({ postits }))
+    }
+
+    handleUpdatePost = (id, text) => {
+        const { userId, token } = this.props
+
+        logic.updatePostit(id, text, userId, token)
+            .then(() => logic.listPostitsByUser(userId, token))
             .then(postits => this.setState({ postits }))
 
-    // TODO error handling!
-
-    handleDeletePost = id =>
-        logic.deletePostit(id)
-            .then(() => logic.listPostits())
-            .then(postits => this.setState({ postits }))
-
-    // TODO error handling!
-
-
-    handleUpdatePost = (id, text) =>
-        logic.updatePostit(id, text)
-            .then(() => logic.listPostits())
-            .then(postits => this.setState({ postits }))
-
-    // TODO error handling!
-
+    }
 
     render() {
+        console.log('Postits', 'render')
+
         return <div>
             <h1>Post-It App <i className="fas fa-sticky-note"></i></h1>
 
