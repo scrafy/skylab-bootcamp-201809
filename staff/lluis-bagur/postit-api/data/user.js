b2e488d96295e7bfe35f91ef4e6b239d322ffe7c
Lluis Bagur <lluisbagur@MacBook-Pro-de-Lluis.local>
2018-11-09 17:35:38 +0100

    connect to mongo

--- staff/lluis-bagur/postit-api/data/user.js
+++ staff/lluis-bagur/postit-api/data/user.js
@@ -14,26 +14,14 @@ class User {
     }
 
     save() {
-        return new Promise((resolve, reject) => {
-            fs.readFile(User._file, (err, json) => {
-                if (err) return reject(err)
-
-                const users = JSON.parse(json)
-
-                const index = users.findIndex(user => user.id === this.id)
-
-                if (index < 0) users.push(this)
-                else users[index] = this
-
-                json = JSON.stringify(users)
-
-                fs.writeFile(User._file, json, err => {
-                    if (err) return reject(err)
-
-                    resolve()
-                })
+        return User._collection.findOne({ id: this.id })
+            .then(user => {
+                if (user) {
+                    return User._collection.updateOne({ id: this.id }, { $set:this})
+                } else {
+                    return User._collection.insertOne(this)
+                }
             })
-        })
     }
 
     toObject() {
@@ -42,35 +30,18 @@ class User {
         return { name, surname, username, password, postits }
     }
 
-    static findByUsername(username) {
-        return new Promise((resolve, reject) => {
-            fs.readFile(User._file, (err, json) => {
-                if (err) return reject(err)
+    static findByUsername(_username) {
+        return User._collection.findOne({ username: _username })
+            .then(user => new User(user))
+             }
 
-                const users = JSON.parse(json)
 
-                const user = users.find(user => user.username === username)
-
-                resolve(user ? new User(user) : undefined)
-            })
-        })
-    }
-
-    static findById(id) {
-        return new Promise((resolve, reject) => {
-            fs.readFile(User._file, (err, json) => {
-                if (err) return reject(err)
-
-                const users = JSON.parse(json)
-
-                const user = users.find(user => user.id === id)
-
-                resolve(user ? new User(user) : undefined)
-            })
-        })
-    }
+    static findById(_id) {
+        return User._collection.findOne({ id: _id })
+        .then(user => new User(user))
+    } 
 }
 
-User._file = './data/users.json'
+User._collection = 'NO-COLLECTION'
 
 module.exports = User
--- staff/lluis-bagur/postit-api/data/user.spec.js
+++ staff/lluis-bagur/postit-api/data/user.spec.js
@@ -1,10 +1,25 @@
-const fs = require('fs')
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb')
 const { expect } = require('chai')
 const { User } = require('.')
 
+const { env: { MONGO_URL } } = process
+
 describe('User (model)', () => {
+    let client, users
+
     before(() => {
-        User._file = './data/users.spec.json'
+        client = new MongoClient(MONGO_URL, { useNewUrlParser: true })
+
+        return client.connect()
+            .then(() => {
+                const db = client.db('postit-test')
+
+                users = db.collection('users')
+
+                User._collection = users
+            })
     })
 
     describe('save', () => {
@@ -16,21 +31,16 @@ describe('User (model)', () => {
             username = `username-${Math.random()}`
             password = `password-${Math.random()}`
 
-            fs.writeFileSync(User._file, JSON.stringify([]))
+            return users.deleteMany()
         })
 
         it('should succeed on correct data', () =>
             new User({ name, surname, username, password }).save()
-                .then(() => {
-                    const json = fs.readFileSync(User._file)
+                .then(() => users.find().toArray())
+                .then(_users => {
+                    expect(_users.length).to.equal(1)
 
-                    const users = JSON.parse(json)
-
-                    debugger
-
-                    expect(users.length).to.equal(1)
-
-                    const [user] = users
+                    const [user] = _users
 
                     expect(user.name).to.equal(name)
                     expect(user.surname).to.equal(surname)
@@ -52,42 +62,38 @@ describe('User (model)', () => {
 
                 id = user.id
 
-                fs.writeFileSync(User._file, JSON.stringify([user]))
+                return user.save()
             })
 
             it('should succeed on correct username', () => {
-                let json = fs.readFileSync(User._file)
+                return users.find().toArray()
 
-                let users = JSON.parse(json)
+                    .then(_users => {
+                        expect(_users.length).to.equal(1)
 
-                expect(users.length).to.equal(1)
+                        const [user] = _users
 
-                let [user] = users
+                        expect(user).to.exist
 
-                expect(user).to.exist
+                        expect(user.name).to.equal(name)
+                        expect(user.surname).to.equal(surname)
+                        expect(user.username).to.equal(username)
+                        expect(user.password).to.equal(password)
 
-                expect(user.name).to.equal(name)
-                expect(user.surname).to.equal(surname)
-                expect(user.username).to.equal(username)
-                expect(user.password).to.equal(password)
+
+                    })
 
                 const newName = `${name}-${Math.random()}`
 
                 const _user = new User({ name: newName, surname, username, password })
 
-                _user.id = id
-
                 return _user.save()
-                    .then(() => {
-                        json = fs.readFileSync(User._file)
 
-                        users = JSON.parse(json)
+                    .then(() => users.find().toArray())
+                    .then(_users => {
+                        expect(_users.length).to.equal(1)
 
-                        expect(users.length).to.equal(1)
-
-                        user = users[0]
-
-                        expect(user).to.exist
+                        const [user] = _users[0]
 
                         expect(user.name).to.equal(newName)
                         expect(user.surname).to.equal(surname)
@@ -107,7 +113,9 @@ describe('User (model)', () => {
             username = `username-${Math.random()}`
             password = `password-${Math.random()}`
 
-            fs.writeFileSync(User._file, JSON.stringify([new User({ name, surname, username, password })]))
+            const user = new User({ name, surname, username, password })
+
+            return user.save()
         })
 
         it('should succeed on correct username', () =>
@@ -122,6 +130,36 @@ describe('User (model)', () => {
                     expect(user.password).to.equal(password)
                 })
 
+        )
+    })
+
+    describe('findById', () => {
+        let id, name, surname, username, password
+
+        beforeEach(() => {
+            name = `name-${Math.random()}`
+            surname = `surname-${Math.random()}`
+            username = `username-${Math.random()}`
+            password = `password-${Math.random()}`
+
+            const user = new User({ name, surname, username, password })
+
+            id = user.id
+
+            return user.save()
+        })
+
+        it('should succeed on correct ID', () =>
+            User.findById(id)
+                .then(user => {
+                    expect(user).to.exist
+                    expect(user).to.be.instanceOf(User)
+
+                    expect(user.name).to.equal(name)
+                    expect(user.surname).to.equal(surname)
+                    expect(user.username).to.equal(username)
+                    expect(user.password).to.equal(password)
+                })
         )
     })
 })
