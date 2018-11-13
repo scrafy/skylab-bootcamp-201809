4a643780519ffeeb15e560cbd5f6fb750c8ac477
Lluis Bagur <lluisbagur@MacBook-Pro-de-Lluis.local>
2018-11-13 12:09:28 +0100

    deleted trash

--- staff/lluis-bagur/postit-app/.versions/postit-app-2.1.0/src/components/Error.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Error(props) {
-    return <p className="error">{props.message}</p>
-}
-
-export default Error
--- staff/lluis-bagur/postit-app/.versions/postit-app-2.2.0/src/components/Landing.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Landing(props) {
-    return <section>
-        <button onClick={props.onRegisterClick}>Register</button> or <button onClick={props.onLoginClick}>Login</button>
-    </section>
-}
-
-export default Landing
