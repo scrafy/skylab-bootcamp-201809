52f6bd38acdc8178246e5965f952f3759e342f2b
scrafy <scrafy26@gmail.com>
2018-11-29 21:08:44 +0100

    29/11/2018

--- staff/joseluis-juste/hive-studio/back/app/Controllers/Http/FarmController.js
+++ staff/joseluis-juste/hive-studio/back/app/Controllers/Http/FarmController.js
@@ -27,18 +27,6 @@ class FarmController extends BaseController {
 
     }
 
-    async index({ auth, request, response }) {
-
-        const { id } = await auth.getUser()
-        const user = await User.find(id)
-        if (!user) {
-            throw new ResourceNotFoundException(`The user with the id ${id} not exists`, 404)
-        }
-        const farms = await user.farms().fetch()
-        this.sendResponse(response, farms)
-
-    }
-
     async delete({ auth, request, response }) {
 
         const { id } = await auth.getUser()
@@ -96,7 +84,6 @@ class FarmController extends BaseController {
         if (!farm) {
             throw new ResourceNotFoundException(`The farm with the id ${farmId} not exists`, 404)
         }
-        
         if (farm.user_id !== Number(id)) {
             throw new ResourceNotFoundException(`The farm with the id ${farmId} does not belongs to the user with the id ${id}`, 404)
         }
--- staff/joseluis-juste/hive-studio/back/start/farm-routes.js
+++ staff/joseluis-juste/hive-studio/back/start/farm-routes.js
@@ -1,7 +1,6 @@
 const Route = use('Route')
 
 Route.get('/find/:farmId', 'FarmController.find').prefix('farm').middleware(['auth'])
-Route.get('', 'FarmController.index').prefix('farm').middleware(['auth'])
 Route.put('/:farmId', 'FarmController.update').prefix('farm').formats(['json']).middleware(['auth']).validator('farm/Farm')
 Route.delete('/:farmId', 'FarmController.delete').prefix('farm').middleware(['auth'])
 Route.post('', 'FarmController.create').prefix('farm').formats(['json']).middleware(['auth']).validator('farm/Farm')
